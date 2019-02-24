3998a99919b043f7792ffd174b765ad678982d94
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-02-24 14:40:52 -0500

    Correccion estetica al codigo

--- view/referencias/istec_js.php
+++ view/referencias/istec_js.php
@@ -7,20 +7,20 @@
  */
 ?>
 
-<script src="./js/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
-<script src="./js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <script src="./js/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
+    <script src="./js/bootstrap.bundle.min.js" type="text/javascript"></script>
 
-<script>
-    setInterval('borde()',1);
-    function borde() {
-        if (screen.width < 992) {
-            $('#navbarText').addClass('border-top');
-            $('#navbarText').addClass('borde-sup');
-            $('#mt-inicio').addClass('borde-sup');
-        } else {
-            $('#navbarText').removeClass('border-top');
-            $('#navbarText').removeClass('borde-sup');
-            $('#mt-inicio').removeClass('borde-sup');
+    <script>
+        setInterval('borde()',1);
+        function borde() {
+            if (screen.width < 992) {
+                $('#navbarText').addClass('border-top');
+                $('#navbarText').addClass('borde-sup');
+                $('#mt-inicio').addClass('borde-sup');
+            } else {
+                $('#navbarText').removeClass('border-top');
+                $('#navbarText').removeClass('borde-sup');
+                $('#mt-inicio').removeClass('borde-sup');
+            }
         }
-    }
-</script>
+    </script>
