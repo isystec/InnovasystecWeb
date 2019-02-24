63ce77688cc4eb46e8d33c2cda81e9a387bb764a
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-02-23 20:25:37 -0500

    Se agrega favicon

--- view/body/header/istec_header.php
+++ view/body/header/istec_header.php
@@ -9,8 +9,8 @@
     <header class="border">
         <section class="container">
             <section class="row justify-content-lg-end">
-                <a class="btn redSocial-Facebook" href=""><i class="fab fa-facebook"></i></a>
-                <a class="btn redSocial-Instagram" href=""><i class="fab fa-instagram"></i></a>
+                <a class="btn redSocial-Facebook" href="https://www.facebook.com/innovasystec/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a class="btn redSocial-Instagram" href="https://www.instagram.com/innovasystec/" target="_blank"><i class="fab fa-instagram"></i></a>
                 <a class="btn redSocial-Twiter" href=""><i class="fab fa-twitter"></i></a>
             </section>
         </section>
