8de04f940f4a5f4e8d9183c4adc97d29196561db
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-02-25 02:29:54 -0500

    Se agrega contenido de servicion en cards

---
+++ view/body/contenido_servicios/istec_services_inicio.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 25/02/2019
+ * Time: 1:20
+ */
+?>
+
+
+    <div class="container mb-5 mt-4">
+
+        <section class="text-center">
+            <h2 class="text-services mb-3">Nuestros Servicios</h2>
+            <p class="linea_de_subrayado"></p>
+        </section>
+        </section>
+        <div class="card-deck">
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Hosting y Dominio</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
+
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Paginas Web</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
+
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Redes y Conectividad</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
