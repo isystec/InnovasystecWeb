6961982deeaade07f2ef8d7596e2e1374d85cd71
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-02-25 01:08:38 -0500

    Se actualiza los estilos del footer

--- view/body/footer/istec_footer.php
+++ view/body/footer/istec_footer.php
@@ -1,28 +1,31 @@
-<footer class="bg-light text-center">
-    <div class="py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col col-sm-12 py-2">
-                    <p class="lead">¿NOS CUENTAS TU PROYECTO?</p>
-                    Tel: (+51) 999 999 999 <br>
-                    E-mail: innovasystec@gmail.com
-                </div>
-                <div class="col-md-4 col-sm-6 py-2">
-                    <p class="lead">SIGUENOS EN REDES!</p>
-                    <a href="https://www.facebook.com/innovasystec/" class="btn btn-outline-dark text-black m-1" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
-                    <a href="https://www.instagram.com/innovasystec/" class="btn btn-outline-dark text-black m-1" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
-                </div>
-                <div class="col-md-3 col-sm-6 py-2">
-                    <p class="lead">¿QUÉ HACEMOS?</p>
-                    Desarrollo Sistemas Web<br>
-                    Hosting & Dominios<br>
-                    Consultora Informática<br>
-                    Redes y Conectividad
+
+    <footer class="detalles_footer text-center">
+        <div class="py-3 mb-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col col-sm-12 py-2">
+                        <p class="lead titulos_footer">¿NOS CUENTAS TU PROYECTO?</p>
+                        Tel: (+51) 999 999 999 <br>
+                        E-mail: innovasystec@gmail.com
+                    </div>
+                    <div class="col-md-4 col-sm-6 py-2">
+                        <p class="lead titulos_footer">SIGUENOS EN REDES!</p>
+                        <a href="https://www.facebook.com/innovasystec/" class="btn btn-outline-light text-white m-1" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
+                        <a href="https://www.instagram.com/innovasystec/" class="btn btn-outline-light text-white m-1" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
+                    </div>
+                    <div class="col-md-4 col-sm-6 py-2">
+                        <p class="lead titulos_footer">¿QUÉ HACEMOS?</p>
+                        Desarrollo de Sistemas Web<br>
+                        Hosting & Dominios<br>
+                        Consultora Informática<br>
+                        Redes y Conectividad
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="py-3">
-        Copyright © 2019 <a href="https://innovasystec.com/" target="_blank">InnovaSysTec</a>. Todos los derechos reservados.
-    </div>
-</footer>
+        <div class="footer_botom">
+            <div class="container py-3 ">
+                Copyright © 2019 <a class="link_footer" href="https://innovasystec.com/" target="_blank">InnovaSysTec</a>. Todos los derechos reservados.
+            </div>
+        </div>
+    </footer>
