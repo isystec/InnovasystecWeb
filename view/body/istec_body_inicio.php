81c671691100b153008ebe2cd6d8a73cbba56a0b
Jorge <jorge14.jlrr@gmail.com>
2019-02-24 20:01:01 -0500

    Se agregar footer

--- view/body/istec_body_inicio.php
+++ view/body/istec_body_inicio.php
@@ -13,6 +13,7 @@
 <?php include "./body/header/istec_header.php";?>
 <?php include "./body/navbar/istec_navbar_inicio.php"?>
 <?php include "./body/slider/istec_slider_inicio.php"?>
+<?php include "./body/footer/istec_footer.php"?>
 
 <?php include "./referencias/istec_js.php";?>
 </body>
