9aa09db19bc1c55f7c0b6a23a5df6c77d892c8c1
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-02-27 01:57:44 -0500

    Se cambia nombre carpetas y corrige referencias

---
+++ view/body/banner/istec_banner_nosotros.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 27/02/2019
+ * Time: 1:55
+ */
--- view/body/istec_body_nosotros.php
+++ view/body/istec_body_nosotros.php
@@ -2,7 +2,7 @@
 
 <?php include "./body/header/istec_header.php";?>
 <?php include "./body/navbar/istec_navbar_nosotros.php"?>
-<?php include "./body/contenido_servicios/istec_services_nosotros.php"?>
+<?php include "./body/contenido/istec_services_nosotros.php"?>
 <?php include "./body/footer/istec_footer.php"?>
 <?php include "./referencias/istec_js.php";?>
 </body>
