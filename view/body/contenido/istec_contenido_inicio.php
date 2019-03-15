ea053bd47358c30b8baf99d17d6027b3038528a6
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-03-15 04:38:25 -0500

    Avance de texto del inicio

--- view/body/contenido/istec_contenido_inicio.php
+++ view/body/contenido/istec_contenido_inicio.php
@@ -13,34 +13,58 @@
         <section class="text-center">
             <h2 class="text-services mb-3">Nuestros Servicios</h2>
             <p class="linea_de_subrayado"></p>
+            <div class="card">
+                <div class="card-body">
+                    <p>Tenemos a dispocision una variedad de servicios que ofrecerle.</p>
+                </div>
+            </div>
         </section>
         </section>
         <div class="card-deck">
             <div class="card">
                 <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Hosting y Dominio</h5>
-                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
-
-                    </p>
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Hosting</h5>
+                    <p class="card-text">Ideal para aquellos que desean iniciar un sitio web en Internet y precisan una herramienta sencilla de utilizar ademas de fácil administración.</p>
                 </div>
             </div>
             <div class="card">
                 <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Paginas Web</h5>
-                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
-
-                    </p>
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Dominios</h5>
+                    <p class="card-text">Único, exclusivo e indispensable para un sitio web ya que a través de él identificarán tu marca y reconocerán su valor e importancia.</p>
                 </div>
             </div>
             <div class="card">
                 <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Redes y Conectividad</h5>
-                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.
-
-                    </p>
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Sitios Web</h5>
+<!--                    <p class="card-text">A través de un sitio web das a conocer tus ideas, conocimientos, servicios o producto al público mundial alcanzando nuevos mercados y opotunidades.</p>-->
+                    <p class="card-text">Fundamental para quienes desean dar a conocer su marca a nivel mundial ganando personalidad, seriedad y profesionalismo.</p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="card-deck">
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Conectividad y Redes</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Sistemas Web</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title negrita">Soporte y Asesoria</h5>
+                    <p class="card-text">El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.</p>
                 </div>
             </div>
         </div>
