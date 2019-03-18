95be5a5eff6eb873098c7c9c76dc3a3a60c7fced
Jorge <jorge14.jlrr@gmail.com>
2019-03-18 15:05:32 -0500

    Se agrega texto en la pag nosotros

--- view/body/contenido/istec_contenido_nosotros.php
+++ view/body/contenido/istec_contenido_nosotros.php
@@ -4,25 +4,21 @@
             <h2 class="text-services mb-3">Quienes somos</h2>
             <p class="linea_de_subrayado"></p>
             <section class="container">
-                <section class="text-left formato-about">
+                <section class="text-justify formato-about">
                     <section class="row justify-content-center">
                         <p class="col-md-7">
-                            Refinery29 es una innovadora compañía de medios digitales y entretenimiento para mujeres jóvenes con una huella de audiencia global de 550 millones en todas las plataformas.
+                            Somos una agencia informática dedicada a brindar distintos servicios con el objetivo de cumplir con las necesidades de nuestros clientes.
                         </p>
                     </section>
                     <section class="row justify-content-center">
                         <p class="col-md-7">
-                            Entregamos estilo y contenido a una audiencia que está altamente comprometida con una variedad de dispositivos y destinos, desde videos y funciones originales hasta experiencias en vivo y narraciones sociales. Nuestra audiencia está compuesta por soñadores decididos que son inteligentes y apasionados, siempre buscando el potencial completo en cada aspecto de sus vidas.
+                            Entregamos mucho compromiso al momento de empezar nuevos proyectos, y es por eso que te queremos ayudar a desarrollar la idea que tengas en mente.
                         </p>
                     </section>
                     <section class="row justify-content-center">
                         <p class="col-md-7">
-                            Como la compañía de medios para mujeres de la próxima generación, hemos cambiado la forma en que el contenido y la narración hablan y representan a las mujeres, siempre provocando las conversaciones que más importan. Cumpliendo con esa misión, hemos impulsado iniciativas impulsadas por la intención como Shatterbox Anthology, una serie de cortometrajes dedicada a aumentar las oportunidades para las mujeres en el cine; El 67%, dedicado a más y mejor representación de talla grande e igualdad; 29Rooms, una casa de juegos con energía artística para la era digital que reinventa los eventos en vivo; y Strong Opinions Loosely Held, donde combinamos cultura pop y feminismo.
-                        </p>
-                    </section>
-                    <section class="row justify-content-center">
-                        <p class="col-md-7">
-                            Celebramos las imperfecciones, desterramos los tabúes y siempre nos esforzamos por ser reales y fáciles de relacionar: caminar en todo lo que hacemos.
+                              Innovasystec cuenta con un equipo altamente calificado para atender ante cualquier inquietud que tengas, y poner en ejecución nuestros servicios brindados
+                              en tu negocio o empresa.
                         </p>
                     </section>
                 </section>
@@ -36,7 +32,7 @@
                     <div class="">
                         <div class="">
                             <strong class="formatoNosotrosTitulos">Nuestra misión</strong>
-                            <p class="formatoNosotrosTexto">Refinery29 es un catalizador para que las mujeres sientan, vean y reclamen su poder.</p>
+                            <p class="formatoNosotrosTexto">Innovasystec tiene como misión dar soluciones informáticas ante cualquier dificultad que se presente en personas y/o empresas.</p>
                         </div>
                     </div>
                 </div>
@@ -44,7 +40,7 @@
                     <div class="">
                         <div class="">
                             <strong class="formatoNosotrosTitulos">Nuestra esencia</strong>
-                            <p class="formatoNosotrosTexto">En nuestro núcleo, Refinery29 opera en Imaginación, Individualidad, Inclusión e Impacto.</p>
+                            <p class="formatoNosotrosTexto">Lo que caracteriza a Innovasystec es la responsabilidad y la dedicación que brinda al momento de trabajar con sus clientes.</p>
                         </div>
                     </div>
                 </div>
@@ -53,7 +49,7 @@
                     <div class="">
                         <div class="">
                             <strong class="formatoNosotrosTitulos">Nuestra promesa</strong>
-                            <p class="formatoNosotrosTexto">Ofrecemos narraciones, experiencias y puntos de vista optimistas y diversos a nuestra audiencia de mujeres inteligentes, curiosas y apasionadas.</p>
+                            <p class="formatoNosotrosTexto">Dar el maximo de nuestras capacidades para poder ayudarte en tu idea de negocio, y llevarla al mundo del internet.</p>
                         </div>
                     </div>
                 </div>
@@ -61,7 +57,7 @@
                     <div class="">
                         <div class="">
                             <strong class="formatoNosotrosTitulos">Nuestro ambiente</strong>
-                            <p class="formatoNosotrosTexto">En Refinery29, hacemos magia. Lo soñamos, y luego lo hacemos, juntos, reinventando cada día lo que es posible.</p>
+                            <p class="formatoNosotrosTexto">En Innovasystec, hacemos magia. Lo soñamos, y luego lo hacemos, juntos, reinventando cada día lo que es posible.</p>
                         </div>
                     </div>
                 </div>
