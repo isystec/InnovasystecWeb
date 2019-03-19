ac04759f6ea703f7fbcdf7fb40fe5127671b18bc
Jorge <jorge14.jlrr@gmail.com>
2019-03-18 20:30:56 -0500

    Se agrega imagenes de cards

--- view/body/contenido/istec_contenido_inicio.php
+++ view/body/contenido/istec_contenido_inicio.php
@@ -22,21 +22,21 @@
         </section>
         <div class="card-deck">
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-hosting.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Hosting</h5>
                     <p class="card-text">Ideal para aquellos que desean iniciar un sitio web en Internet y precisan una herramienta sencilla de utilizar ademas de fácil administración.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-dominio.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Dominios</h5>
                     <p class="card-text">Único, exclusivo e indispensable para un sitio web ya que a través de él identificarán tu marca y reconocerán su valor e importancia.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-desarrolloweb.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Desarrollo Web</h5>
                     <p class="card-text">Fundamental para quienes desean dar a conocer su marca a nivel mundial ganando personalidad, seriedad y profesionalismo.</p>
@@ -46,21 +46,21 @@
         <br>
         <div class="card-deck">
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-redes.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Conectividad y Redes</h5>
                     <p class="card-text">Es una gran ventaja el hacer uso de redes informaticas ya que se obtiene un gran ahorro de recursos asi como el evitar gastos innecesarios.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-sistemas.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Sistemas Web</h5>
                     <p class="card-text">Serás capaz de acceder, realizar operaciones, generar reportes y ver resultados de forma segura solo con tener conexión a internet.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="./img/card_ejemplo.jpg" class="card-img-top" alt="...">
+                <img src="./img/card-consultoria.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title negrita">Soporte y Asesoria</h5>
                     <p class="card-text">Cada uno de nuestros productos o servicios incluyen el derecho a solicitar soporte relacionado a las tecnologías de la información.</p>
