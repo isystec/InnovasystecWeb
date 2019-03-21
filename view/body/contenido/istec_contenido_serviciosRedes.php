20dd330840977618370a67a0c14f3849354a7da0
jorgejlrr <jorge14.jlrr@gmail.com>
2019-03-20 20:47:06 -0500

    Se termina las tarifas redes y sistemas

--- view/body/contenido/istec_contenido_serviciosRedes.php
+++ view/body/contenido/istec_contenido_serviciosRedes.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alejandro
- * Date: 12/03/2019
- * Time: 22:25
- */
+
+<div class="pricingTable">
+        <ul class="pricingTable-firstTable">
+            <li class="pricingTable-firstTable_table">
+                <p class="pricingTable-firstTable_table__pricing"><span></span><span>Cotizar</span> <br> <span> x Magnitud de la red</span></p>
+                <ul class="pricingTable-firstTable_table__options">
+                    <li>Número de PC´s</li>
+                    <li>Área (metros cuadrados)</li>
+                    <li>Configuración</li>
+                    <li>Diseño</li>
+                    <li>Instalación</li>
+                </ul>
+                <div class="pricingTable-firstTable_table__getstart" onclick="location.href='contacto.php#contactanos'">Solicitar</div>
+            </li>
+        </ul>
+</div>
